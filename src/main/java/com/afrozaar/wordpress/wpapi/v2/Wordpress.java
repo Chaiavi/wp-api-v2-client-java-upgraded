4a1d3772b244a56fe0451893aef2b0e487222eb5
Chaiavi <avraham2@gmail.com>
2025-02-21 15:32:43 +0200

    Added comment updation. #60
    following this pull request fix:
    https://github.com/Afrozaar/wp-api-v2-client-java/pull/60/commits/3ad60be2c5f35c248680b3159701b571e1c86d6a

--- src/main/java/com/afrozaar/wordpress/wpapi/v2/Wordpress.java
+++ src/main/java/com/afrozaar/wordpress/wpapi/v2/Wordpress.java
@@ -1,22 +1,13 @@
 package com.afrozaar.wordpress.wpapi.v2;
 
-import com.afrozaar.wordpress.wpapi.v2.api.Categories;
-import com.afrozaar.wordpress.wpapi.v2.api.CustomCalls;
-import com.afrozaar.wordpress.wpapi.v2.api.Medias;
-import com.afrozaar.wordpress.wpapi.v2.api.Pages;
-import com.afrozaar.wordpress.wpapi.v2.api.PostMetas;
-import com.afrozaar.wordpress.wpapi.v2.api.Posts;
-import com.afrozaar.wordpress.wpapi.v2.api.Tags;
-import com.afrozaar.wordpress.wpapi.v2.api.Taxonomies;
-import com.afrozaar.wordpress.wpapi.v2.api.Terms;
-import com.afrozaar.wordpress.wpapi.v2.api.Users;
+import com.afrozaar.wordpress.wpapi.v2.api.*;
 import com.afrozaar.wordpress.wpapi.v2.request.SearchRequest;
 import com.afrozaar.wordpress.wpapi.v2.response.PagedResponse;
 
 import java.net.URI;
 import java.util.function.Function;
 
-public interface Wordpress extends Posts, PostMetas, Taxonomies, Terms, Medias, Pages, Users, Tags, Categories, CustomCalls {
+public interface Wordpress extends Posts, Comments, PostMetas, Taxonomies, Terms, Medias, Pages, Users, Tags, Categories, CustomCalls {
 
     String getContext();
 
